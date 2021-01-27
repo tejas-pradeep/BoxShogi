796f416e876f9e3628ed9ad8276861af5a55084f
rptejas@gmail.com <Ingaman01*>
2021-01-27 11:31:02 -0600

    Creating Piece abstract class

--- piece.py
+++ piece.py
@@ -1,10 +1,23 @@
-class Piece:
+import abc
+can_promote = set(['r', 'g', 'n', 'p'])
+class Piece(metaclass=abc.ABCMeta):
     """
     Class that represents a BoxShogi piece
     """
 
-    def __init__(self):
+    def __init__(self, type, index):
+        self.type = type
+        self.x = index[0]
+        self.y = index[1]
         pass
 
     def __repr__(self):
         return ""
+
+    @abc.abstractmethod
+    def getMoves(self):
+        pass
+
+    @abc.abstractmethod
+    def promote(self):
+        pass
