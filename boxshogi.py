25c6b3cc400e7c4c85ddf942ca5c0171cdb1ae55
tpradeep8 <rptejas@sametunes.com>
2021-02-03 00:24:22 -0500

    Fixing commented code in boxshogi.py

--- boxshogi.py
+++ boxshogi.py
@@ -97,13 +97,13 @@ def main():
     Main function to read terminal input
     Calls method parseTestCase() from utils that takes in file path and returns a dictionary of the file data.
     """
-    game('f', parseTestCase('test_cases/promoteLeavingZone.in'))
+    # game('f', parseTestCase('test_cases/promoteLeavingZone.in'))
     # game('i')
-    # if sys.argv[1] == '-f':
-    #     input = parseTestCase(sys.argv[2])
-    #     game('f', input)
-    # if sys.argv[1] == '-i':
-    #     game('i')
+    if sys.argv[1] == '-f':
+        input = parseTestCase(sys.argv[2])
+        game('f', input)
+    if sys.argv[1] == '-i':
+        game('i')
 
 
 if __name__ == "__main__":
